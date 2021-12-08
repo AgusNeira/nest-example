2351a34dcc02829148c4d7156a8297efebc731fd
AgusNeira <a2002neira@gmail.com>
2021-12-08 15:50:12 -0300

    [ADD] Basic frontend implementation

--- SQL_INIT.sql
+++ SQL_INIT.sql
@@ -1,4 +1,4 @@
 CREATE DATABASE IF NOT EXISTS nest_app;
 USE nest_app;
-CREATE TABLE IF NOT EXISTS nest_app.users (id int(4), name VARCHAR(20), DNI INT(9), gender VARCHAR(1), birth VARCHAR(20));
+CREATE TABLE IF NOT EXISTS nest_app.users (id int(4) AUTO_INCREMENT, name VARCHAR(20), DNI INT(9), gender VARCHAR(1), birth VARCHAR(20), PRIMARY KEY (id));
 
